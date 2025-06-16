27584499c5d4f551855edb6694bb299708577141
Jocaagura <oficinameltablero@gmail.com>
2025-06-16 08:53:54 -0500

    feat(@albertjjimenezp): initialize Planning Poker project with clean architecture and SOLID principles using Windsurf

---
+++ bin/ppdartw.dart
@@ -0,0 +1,5 @@
+import 'package:ppdartw/ppdartw.dart' as ppdartw;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ppdartw.calculate()}!');
+}
---
+++ test/ppdartw_test.dart
@@ -0,0 +1,8 @@
+import 'package:ppdartw/ppdartw.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
